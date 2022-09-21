307a0249adb545e1f821189a1dc0062b1dc6f674
Sushrut1101 <guptasushrut@gmail.com>
2022-09-21 21:08:41 +0530

    Update
    * Run in the fg mode if possible

--- tmate.sh
+++ tmate.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+
+PROJECT_DIR=$(pwd)
+
+# Change to $HOME
 cd ~
-tmate -S /var/tmate.sock new-session -d               # Launch tmate in a headless mode
-tmate -S /var/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
-tmate -S /var/tmate.sock display -p '#{tmate_ssh}'    # Prints the SSH connection string
-sleep 3
+
+# Run tmate
+tmate -F || {
+    tmate -S /var/tmate.sock new-session -d               # Launch tmate in a headless mode
+    tmate -S /var/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
+    tmate -S /var/tmate.sock display -p '#{tmate_ssh}'    # Prints the SSH connection string
+    sleep 3
+
+    # Run the Blank Output script
+    bash ${PROJECT_DIR}/blank.sh
+}
+
+# Exit
+exit 0
